0b28b40b4f63ff638178b34e9255d52fbb6ffa73
justsofico <justsofi@ukr.net>
2026-07-21 19:11:47 +0300

    Create document.py

---
+++ document.py
@@ -0,0 +1,19 @@
+class Document:
+
+    def __init__(
+
+        self,
+
+        name,
+
+        size,
+
+        checksum
+
+    ):
+
+        self.name = name
+
+        self.size = size
+
+        self.checksum = checksum
