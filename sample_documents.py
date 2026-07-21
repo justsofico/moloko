9d07c5f714b9f41599f2ac1a78daac48c06478aa
justsofico <justsofi@ukr.net>
2026-07-21 19:12:22 +0300

    Create sample_documents.py

---
+++ sample_documents.py
@@ -0,0 +1,65 @@
+from document import Document
+
+DOCUMENTS = [
+
+    Document(
+
+        "contract.pdf",
+
+        124000,
+
+        "AAA111"
+
+    ),
+
+    Document(
+
+        "contract_copy.pdf",
+
+        124000,
+
+        "AAA111"
+
+    ),
+
+    Document(
+
+        "notes.txt",
+
+        2400,
+
+        "BBB222"
+
+    ),
+
+    Document(
+
+        "notes_backup.txt",
+
+        2400,
+
+        "BBB222"
+
+    ),
+
+    Document(
+
+        "manual.docx",
+
+        82000,
+
+        "CCC333"
+
+    ),
+
+    Document(
+
+        "report.docx",
+
+        91000,
+
+        "DDD444"
+
+    )
+
+]
